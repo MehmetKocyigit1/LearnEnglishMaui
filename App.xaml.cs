ec9ceee680be6c6d6c8d03eb8928354194b2383a
MehmetKocyigit1 <hmkcygt4@gmail.com>
2026-07-05 01:33:18 +0300

    feat: force light theme globally by default and remove hardcoded dark shell header colors

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,12 @@
-﻿namespace IngilizceProjeMAUI
+namespace IngilizceProjeMAUI
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            UserAppTheme = AppTheme.Light; // Force Light Mode (White background) by default
 
             MainPage = new AppShell();
         }
